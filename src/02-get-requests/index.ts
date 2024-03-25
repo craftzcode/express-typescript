ccd2ea2369dbc433a6501e218d5a1c78e3ecbf76
igmtdev <igmtdevofficial@gmail.com>
2024-03-25 23:15:31 +0800

    added 02-get-requests

---
+++ src/02-get-requests/index.ts
@@ -0,0 +1,44 @@
+import express from 'express'
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+//! (req, request) To get a data from (body), to access (cookies), or to access (headers)
+//! (res || response) To send back data, text, html, or json to the user
+app.get('/', (req, res) => {
+  // Text
+  // res.send('Hello, World')
+
+  // Json
+  // res.send({ message: 'Hello' })
+
+  // Status + Json
+  res.status(200).send({ message: 'Hello' })
+})
+
+//! (http://localhost:3000/v1/users) api route
+app.get('/v1/users', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      username: 'ivan',
+      displayName: 'Ivan'
+    },
+    {
+      id: 2,
+      username: 'ali',
+      displayName: 'Ali'
+    },
+    {
+      id: 3,
+      username: 'christine',
+      displayName: 'Christine'
+    }
+  ])
+})
+
+//! (app.listen) Allows you to listen to a (PORT) for incoming requests, this actually start up the (express) server
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+)
